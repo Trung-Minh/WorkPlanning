d019c7ad0429b537b3f67a4532f1c820618af6de
trungdev <trungleminh76@gmail.com>
2025-07-26 22:10:53 +0700

    fix plans

--- app/Http/Controllers/PlansController.php
+++ app/Http/Controllers/PlansController.php
@@ -11,7 +11,13 @@ class PlansController extends Controller
 {
     public function index()
     {
-        $keHoachs = DB::table('KE_HOACH')->get();
+        if (!Auth::check()) {
+            return redirect()->route('login')->with('error', 'Bạn cần đăng nhập!');
+        }
+
+        $keHoachs = DB::table('KE_HOACH')
+            ->where('NGUOI_TAO', Auth::user()->ID_USER)
+            ->get();
 
         foreach ($keHoachs as $keHoach) {
             $keHoach->cong_viec = DB::table('CONG_VIEC')
@@ -27,6 +33,7 @@ class PlansController extends Controller
 
         return view('plans', compact('keHoachs'));
     }
+
 
     public function store(Request $request)
     {
